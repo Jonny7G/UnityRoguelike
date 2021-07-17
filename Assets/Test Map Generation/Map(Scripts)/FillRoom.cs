7c60f6d8cd176e2cea9d57877aa81952e2e8d6d3
kyledicke <50958747+kyledicke@users.noreply.github.com>
2021-07-16 22:58:31 -0400

    added auto-level generation
    All my changes are in the "Test Map Generation" folder. You can see that map it creates in "Test Scene."

---
+++ Assets/Test Map Generation/Map(Scripts)/FillRoom.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FillRoom : MonoBehaviour
+{
+    public LayerMask Proxy; 
+    public LevelGeneration levelGen;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        Collider2D roomDetection = Physics2D.OverlapCircle(transform.position, 1, Proxy); //Fills random room
+        if (roomDetection == null && levelGen.stopGen == true) //Checks if area is empty and spawns room
+        {
+            int rand = Random.Range(0, levelGen.rooms.Length);
+            Instantiate(levelGen.rooms[rand], transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Test Map Generation/Map(Scripts)/SpawnTile.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject[] objects;
+    private void Start()
+    {
+        int rand = Random.Range(0, objects.Length);
+        GameObject instance = Instantiate(objects[rand], transform.position, Quaternion.identity);
+        instance.transform.parent = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
